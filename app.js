2a6a1d7f814010a968ca67b49760b3d7d710ec58
jesusmata10 <jesusmata10@gmail.com>
2021-03-24 03:51:59 -0400

    guardar

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-const express = require('express');
-const app     = express();
 
-//const connection = require('./database/db');
+// 1. invocamos a express
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res)=>{
-	res.send('Hola mundo');
+	res.send('Hola mundo mas');
 });
 
-app.listen(3000, (req, res) => {
-	console.log('Servidor ejecutado en http://localhost:3000');
+// 2. seteamos urlencoded para capturar los datos del formulario
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(express.json);
+// 3. Invocamos el dotenv
+const dotenv = require('dotenv');
+dotenv.config({
+	path: './env/.env'
 });
+// 4. el directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+// 5. establecer el motor de plantilla ejs
+app.set('view engine', 'ejs');
+// 6. Invocamos a bcryptjs
+const bcryptjs = require('bcryptjs');
+// 7. var. session
+const session = require('express-session');
+app.use(session({
+	secret: '12345678',
+	resave: true,
+	saveUninitialized: true
+}));
+// 8. Base de dato
+// 9. Rutas
+/*app.get('', (req, res)=>{
+	res.send('login');
+});*/
+const connection = require('./database/db');
 
-
+app.listen(4567, (req, res) => {
+	console.log('Servidor ejecutado en http://localhost:4567');
+});
