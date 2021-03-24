f7841aef177fe728aa5bb2bc156d33b5deb22a94
jesusmata10 <jesusmata10@gmail.com>
2021-03-23 22:19:02 -0400

    guardar

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app     = express();
 
 //const connection = require('./database/db');
 
+app.get('/', (req, res)=>{
+	res.send('Hola mundo');
+});
+
 app.listen(3000, (req, res) => {
 	console.log('Servidor ejecutado en http://localhost:3000');
 });
