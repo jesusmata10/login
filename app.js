b92d376ff88a44d0f0b26cabfd81008f93b6eed7
jesusmata10 <jesusmata10@gmail.com>
2021-03-25 20:28:10 -0400

    guardar

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-
 // 1. invocamos a express
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-	res.send('Hola mundo mas');
+app.get('/', (req, res) => {
+    res.render('login');
 });
 
 // 2. seteamos urlencoded para capturar los datos del formulario
 app.use(express.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(express.json);
 // 3. Invocamos el dotenv
 const dotenv = require('dotenv');
 dotenv.config({
-	path: './env/.env'
+    path: './env/.env'
 });
 // 4. el directorio public
-app.use('/resources', express.static('public'));
-app.use('/resources', express.static(__dirname + '/public'));
+app.use('/resources', express.static('/public'));
+//app.use('/resources', express.static(__dirname + '/public'));
+
 // 5. establecer el motor de plantilla ejs
+//app.set('views',(__dirname, 'views'));
+//app.set('views', './views');
 app.set('view engine', 'ejs');
 // 6. Invocamos a bcryptjs
 const bcryptjs = require('bcryptjs');
 // 7. var. session
 const session = require('express-session');
 app.use(session({
-	secret: '12345678',
-	resave: true,
-	saveUninitialized: true
+    secret: '12345678',
+    resave: true,
+    saveUninitialized: true
 }));
 // 8. Base de dato
 // 9. Rutas
@@ -38,6 +40,6 @@ app.use(session({
 });*/
 const connection = require('./database/db');
 
-app.listen(4567, (req, res) => {
-	console.log('Servidor ejecutado en http://localhost:4567');
+app.listen(3000, (req, res) => {
+    console.log('Servidor ejecutado en http://localhost:3000');
 });
--- database/db.js
+++ database/db.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
+
 connection.connect((error) => {
-	if (error) {
-		console.log('El error de conexion es : ' + error);
-		return;
-	}
-	console.log('¡Conectado a la base de datos!');
+    if (error) {
+        console.log('El error de conexion es : ' + error);
+        return;
+    }
+    console.log('¡Conectado a la base de datos!');
 });
 module.exports = connection;
