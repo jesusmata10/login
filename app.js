2c3dd32ffaa2f7052e99b0ba4c925988bce763f8
jesusmata10 <jesusmata10@gmail.com>
2021-03-30 01:08:17 -0400

    insert

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
 // 1. invocamos a express
-'use strict';
-
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 // 2. el directorio public
 app.use(express.static('public'));
 app.use(express.static('asset'));
 app.use('/static', express.static(__dirname + '/public'));
 
-// 3. Rutas
-var routeIndex = require('./routers/index');
-app.use('/', routeIndex);
-var userIndex = require('./routers/user')
-app.use('/user', userIndex)
-
-
-
-
-
-
-
-
-
-
-
 // 3. seteamos urlencoded para capturar los datos del formulario
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(express.json);
-// 4. Invocamos el dotenv
+//app.use(express.urlencoded({ extended: false }));
+//app.use(express.json);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
+
+
+// 5. Invocamos el dotenv
 const dotenv = require('dotenv');
-dotenv.config({
+dotenv.config({ 
     path: './env/.env'
 });
 
-// 5. establecer el motor de plantilla ejs
+// 6. establecer el motor de plantilla ejs
 app.set("views", path.resolve(__dirname, "views"));
 app.set('view engine', 'ejs');
-// 6. Invocamos a bcryptjs
+// 7. Invocamos a bcryptjs
 const bcryptjs = require('bcryptjs');
-// 7. var. session
+// 8. var. session
 const session = require('express-session');
 app.use(session({
     secret: '12345678',
     resave: true,
     saveUninitialized: true
 }));
-// 8. Base de datos
+// 9. Base de datos
 const connection = require('./database/db');
 
-// 9. escucha
+// 4. Rutas
+var routeIndex = require('./routers/index');
+app.use('/', routeIndex);
+var userIndex = require('./routers/user')
+app.use('/user', userIndex);
+
+// 10. escucha
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
     console.log('Servidor ejecutado en http://localhost:' + port);
--- routers/index.js
+++ routers/index.js
@@ -1,24 +1,46 @@
 var express = require('express')
 var router = express.Router()
+var bcryptjs = require('bcryptjs');
+const connection = require('../database/db');
 
-// define the home page route
-router.get('/', function(req, res) {
-    res.render('login')
-})
-// define the about route
-router.get('/home', function(req, res) {
-    res.render('index')
-})
 
-router.get('/registro', (req, res) =>{
-	res.render('registro')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index')
 })
 
-router.get('/login', (req, res)=>{
-	res.render('login')
-})
+router.route('/registro')
+    .get(function(req, res) {
+        res.render('registro');
+    })
+    .post(async(req, res, next) => {
+    	
+    	/*console.log(req.body);
+  		res.json(req.body);*/
+        const user = req.body.usuario;
+        const email = req.body.email;
+        const rol = req.body.rol;
+        const pass = req.body.password;
+        let password = await bcryptjs.hash(pass, 8);
+        console.log(password);
+        connection.query('INSERT INTO user SET ?', {user:user, email:email, rol:rol, password:password}, async (error, results) => {
+            if(error){
+            	throw error;	
+            } else {
+            	var msg = 'Mi primera pagina con EJS';
+            	console.log('¡Registro agregado!', results );
+            	res.render('registro', { msg: msg } );
+            }
+            	
+        });
+    })
 
-
+    .put(function(req, res) {
+        res.send('Update the book')
+    })
+    .delete((req, res)=>{
+    	res.send('Update the book')
+    })
 
 
 module.exports = router
